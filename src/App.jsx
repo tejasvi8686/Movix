8386b9dc6a748fabf343e32dd8ec527ce2ad56da
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-24 01:20:10 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,17 @@ function App() {
     fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
 
-      const url ={
+      const url = {
         backdrop: res.images.secure_base_url + "original",
         poster: res.images.secure_base_url + "original",
         profile: res.images.secure_base_url + "original",
-      }
+      };
       dispatch(getApiConfiguration(url));
     });
   };
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
