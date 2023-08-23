6667a713d8a57e6a3650b7ebb5cff80e6fdffade
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-23 15:44:16 +0530

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
-import {fetchDataFromApi } from "./utils/api";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-
-  useEffect (() =>   {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+  useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
-     .then((res) => {
-       console.log(res)
-     })
-  }
-  return <div className="App">App</div>;
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.scss";
+
+import HeroBanner from "./heroBanner/HeroBanner";
+
+HeroBanner;
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <HeroBanner />
+    </div>
+  );
+};
+
+export default Home;
