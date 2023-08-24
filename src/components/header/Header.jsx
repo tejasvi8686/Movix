0a6a8c5314821490600774cfaf68049065d8538b
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-24 21:34:41 +0530

    footer,footer.scss

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
-const location = useLocation();
 
 import "./style.scss";
 
