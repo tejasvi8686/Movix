7cc0d9d6ee2afd15134dd1330469b7d811989d0d
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-24 22:24:19 +0530

    Carousel Section - Switching Tabs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending.jsx/Trending";
 
 HeroBanner;
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <div className="homepage">
       <HeroBanner />
+      <Trending />
     </div>
   );
 };
