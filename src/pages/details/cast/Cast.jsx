b6ab80226f5760930c746d1f2e877c84150bce63
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-26 11:56:21 +0530

    error fixed

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -4,8 +4,9 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
+
 import avatar from "../../../assets/avatar.png";
+import Img from "../../../components/lazyLoadImage/Img";
 
 const Cast = ({ data, loading }) => {
     const { url } = useSelector((state) => state.home);
